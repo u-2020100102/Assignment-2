5fca13a435585de8604aa10d5da6121e0b0fc910
u-2020100102 <changewhilealive@gmail.com>
2024-03-06 00:55:03 +0300

    add the book creation page

---
+++ CetBooks/Controllers/BooksController.cs
@@ -0,0 +1,69 @@
+﻿using CetBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CetBooks.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            FakeDB fakeDB = new FakeDB();
+            var allbooks = fakeDB.GetAllBooks();
+
+            return View(allbooks);
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+
+            FakeDB db = new FakeDB();
+            var book = db.GetBookById(id.Value);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+
+            FakeDB db = new FakeDB();
+            var result = db.DeleteBook(id.Value);
+            if (result)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public IActionResult Create(Book book)
+        {
+            if(book.Title  != null) {
+                FakeDB db = new FakeDB();
+                var length = db.GetAllBooks().Count();
+                var newBook = new Book
+                {
+                    Id = length + 1,
+                    Title = book.Title,
+                    Description = book.Description,
+                    Author = book.Author,
+                    PageSize = book.PageSize,
+                    Price = book.Price,
+                    PublishDate = book.PublishDate
+                };
+                db.AddBook(newBook);
+            }
+            return View();
+        }
+
+    }
+}
